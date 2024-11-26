9185883d8011a5a77bd4be74721682006e76771a
Kirill Gribkov <gribkovkiryusha@yandex.ru>
2024-11-26 15:11:04 +0300

    v4.1

--- easy.py
+++ easy.py
@@ -1,33 +1,62 @@
-from functools import total_ordering
-
-
-@total_ordering
-class Version:
-    def __init__(self, version: str):
-        version: list = version.split('.')
-        while len(version) < 3:
-            version.append('0')
-        self.version = '.'.join(version)
-        self.numbers = list(map(int, version))
+class Matrix:
+    def __init__(self, rows: int, cols: int, value: int = 0):
+        self.rows = rows
+        self.cols = cols
+        if isinstance(value, int):
+            self.matrix = [
+                [value for i in range(self.cols)]
+                for j
+                in range(self.rows)
+            ]
 
     def __str__(self) -> str:
-        return self.version
+        rows = map(lambda x: ' '.join(map(str, x)), self.matrix)
+        return '\n'.join(rows)
 
     def __repr__(self) -> str:
-        return f'Version({repr(self.version)})'
+        return f'Matrix({self.rows}, {self.cols})'
 
-    def __eq__(self, other):
-        if isinstance(other, Version):
-            return self.numbers == other.numbers
-        return NotImplemented
+    def __pos__(self):
+        new_obj = Matrix(self.rows, self.cols)
+        new_obj.matrix = self.matrix
+        return new_obj
 
-    def __lt__(self, other):
-        if isinstance(other, Version):
-            if self.numbers[0] < other.numbers[0]:
-                return self.numbers[0] < other.numbers[0]
-            elif self.numbers[0] == other.numbers[0]:
-                if self.numbers[1] < other.numbers[1]:
-                    return self.numbers[1] < other.numbers[1]
-                elif self.numbers[1] == other.numbers[1]:
-                    return self.numbers[2] < other.numbers[2]
-        return NotImplemented
+    def __neg__(self):
+        new_obj = Matrix(self.rows, self.cols)
+        new_obj.matrix = [
+                [self.matrix[j][i] * -1 for i in range(self.cols)]
+                for j
+                in range(self.rows)
+            ]
+        return new_obj
+
+    def __invert__(self):
+        new_obj = Matrix(self.cols, self.rows)
+        new_obj.matrix = [
+                [self.matrix[i][j] for i in range(self.rows)]
+                for j
+                in range(self.cols)
+            ]
+        return new_obj
+
+    def __round__(self, n = None):
+        new_obj = Matrix(self.rows, self.cols)
+        if n is None:
+            new_obj.matrix = [
+                [round(self.matrix[j][i]) for i in range(self.cols)]
+                for j
+                in range(self.rows)
+            ]
+            return new_obj
+        new_obj.matrix = [
+                [round(self.matrix[j][i], n)  for i in range(self.cols)]
+                for j
+                in range(self.rows)
+            ]
+        return new_obj
+
+    def get_value(self, row: int, col: int) -> int | float:
+        return self.matrix[row][col]
+
+    def set_value(self, row: int, col: int, value: int | float):
+        self.matrix[row][col] = value
--- test_zip.py
+++ test_zip.py
@@ -8,7 +8,7 @@ from easy import *  # Подскажите как избавиться от Flak
 class StrStream:
     '''A brazenly stolen and slightly modified class
        that creates a string of task results to compare
-       the latter with the correct answer.'''
+       the latter with the correct new_name.'''
     def __init__(self):
         self.data = ''
 
@@ -26,29 +26,45 @@ class StrStream:
 def file_transfer_script():
     '''Script function for transferring a file from
     the download folder.
-    To work on your system, change the path to the directories.'''
+    To work on your system,
+    change the path to the directories.'''
 
-    file_source = f'C:\\Users\\{os.getlogin()}\\Downloads\\'
-    file_destination = os.getcwd()
-    os.chdir(file_source)
-    print('Pay attention to the file download.. '
-          'File names may be repeated.')
-    zip_name = f'{input("Enter the name of the file without .zip: ")}.zip'
-    if os.path.exists(zip_name):
-        os.chdir(file_destination)
+    with open('zip_save.txt', 'a+', encoding='utf-8') as file:
+        '''A context manager that overwrites data in
+        zip_save.txt if necessary.'''
+        file.seek(0)
+        old_name = file.readline()
+        if old_name:
+            print(f'To continue working with file {old_name}, press Enter,')
+            new_name = input("otherwise specify the file name without .zip: ")
+        else:
+            new_name = input("Enter the name of the file without .zip: ")
+        if not old_name and not new_name:
+            raise FileNotFoundError('The file is not specified.')
+        elif old_name and not new_name:
+            flag_new_name = False
+        else:
+            flag_new_name = True
+            new_name += '.zip'
+            file.truncate(0)
+            file.write(new_name)
 
-        for filename in os.listdir(file_destination):
-            '''The cycle of deleting unnecessary archives.'''
-            if filename.endswith('.zip'):
-                file_path = os.path.join(file_destination, filename)
-                os.unlink(file_path)
-
-        shutil.move(file_source + zip_name, file_destination)
+    if flag_new_name:
+        file_source = f'C:\\Users\\{os.getlogin()}\\Downloads\\'
+        file_destination = os.getcwd()
+        os.chdir(file_source)
+        if os.path.exists(new_name):
+            os.chdir(file_destination)
+            file_path = os.path.join(file_destination, old_name)
+            os.unlink(file_path)
+            shutil.move(file_source + new_name, file_destination)
+        else:
+            os.chdir(file_destination)
+            if not os.path.exists(f'{new_name}'):
+                raise FileNotFoundError('You have not downloaded the file.')
+        return new_name
     else:
-        os.chdir(file_destination)
-        if not os.path.exists(f'{zip_name}'):
-            raise FileNotFoundError('You have not downloaded the file.')
-    return zip_name
+        return old_name
 
 
 def main():
@@ -61,15 +77,15 @@ def main():
         info = filter(lambda name: name.isdigit(), info)
         for name in info:
             with (zip_file.open(f'{name}', 'r') as input_file,
-                  zip_file.open(f'{name}.clue', 'r') as answer_file):
+                  zip_file.open(f'{name}.clue', 'r') as new_name_file):
                 with StrStream() as x:
                     exec(input_file.read().decode('utf-8'))
                 input_data = x.data
-                answer_data = answer_file.read().decode('utf-8') + '\n'
-                if input_data != answer_data:
-                    print(f'wrong answer in test №{name}')
-                    print(f'your answer:\n{input_data}')
-                    print(f'correct answer:\n{answer_data}')
+                new_name_data = new_name_file.read().decode('utf-8') + '\n'
+                if input_data != new_name_data:
+                    print(f'wrong new_name in test №{name}')
+                    print(f'your new_name:\n{input_data}')
+                    print(f'correct new_name:\n{new_name_data}')
                     break
                 print(f'№{name} - OK!')
         else:
